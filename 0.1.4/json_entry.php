ebd65082f6dbb63e5d01e8cf5dfc93ef82982bf4
Lucas Lima T <lukaslymauchiha@gmail.com>
2019-02-01 22:33:44 -0200

    Add files via upload

---
+++ 0.1.4/json_entry.php
@@ -0,0 +1,37 @@
+<?php
+
+$json = <<<JSON
+{
+    "data": [
+        {
+            "nome": "Lucas Lima",
+            "email": "lucaslimatorre@gmail.com",
+            "telefone": "(11)91234-5678"
+        },
+        {
+            "nome": "Jade Lima",
+            "email": "jade@gmail.com",
+            "telefone": "(11)98765-4321"
+        },
+        {
+            "nome": "Diana Lima",
+            "email": "diana@gmail.com",
+            "telefone": "(11)94321-8765"
+        }
+    ]
+}
+JSON;
+
+$jsonPHP = json_decode($json);
+
+var_dump($jsonPHP);//GET All Json
+
+var_dump($jsonPHP->data);//GET data
+
+var_dump($jsonPHP->data[1]->nome);//GET nome in position [1]
+
+foreach($jsonPHP->data as $cliente){
+    echo $cliente->nome . ' - ';
+}
+
+?>
